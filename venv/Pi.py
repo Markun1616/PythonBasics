24203f212a2044e2ed649e637d4accba6e0abcfa
Markun1616 <61167091+Markun1616@users.noreply.github.com>
2020-02-18 11:36:22 +0100

    Created CommonList

---
+++ venv/Pi.py
@@ -0,0 +1,19 @@
+import math
+
+count = 0
+try:
+    digits = int(input("Enter number of Pi number digits: ")) - 1
+
+    if digits == -1:
+        print("Empty sequence!")
+    elif digits < -1:
+        print("Impossible to generate negative sequence")
+    else:
+        try:
+            while count <= digits:
+                print(str(math.pi)[count], end='')
+                count += 1
+        except IndexError:
+            print("... To many digits")
+except ValueError:
+    print("Please, enter a number")
